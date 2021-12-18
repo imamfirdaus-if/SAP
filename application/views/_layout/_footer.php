95bf4ab5feb04caa2c1cf854dd9d75abee6be3b9
imamfirdaus-if <imamfirdaus3110@gmail.com>
2021-12-19 00:05:58 +0700

    commit-msg

---
+++ application/views/_layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="pull-right hidden-xs">
+		Sistem Administrasi Pegawai
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2021 Kelompok 9 - Imam Firdaus & Nita Amalia</strong> All rights reserved.
+</footer>
---
+++ application/views/_layout/_header.php
@@ -0,0 +1,10 @@
+<header class="main-header">
+  <!-- Logo -->
+  <a href="<?php echo base_url(); ?>" class="logo">
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg">S A P</span>
+  </a>
+
+  <!-- nav -->
+  <?php echo @$_nav; ?>
+</header>
